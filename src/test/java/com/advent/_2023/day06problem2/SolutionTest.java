e3cb9542f0e4d170bdc4ba100ed116aff6e26f7a
mhiginio <mhiginio@gmail.com>
2023-12-09 09:53:46 +0100

    Day 6

---
+++ src/test/java/com/advent/_2023/day06problem2/SolutionTest.java
@@ -0,0 +1,25 @@
+package com.advent._2023.day06problem2;
+
+import com.advent._2023.InputParser;
+import com.advent._2023.StringParser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void example_data_should_return_71503() {
+        String[] data = {
+                "Time:      7  15   30",
+                "Distance:  9  40  200"};
+        assertEquals(71503, solution.solution(new StringParser(data)));
+    }
+
+    @Test
+    void provided_data_should_return_34454850() {
+        assertEquals(34454850,
+                solution.solution(new InputParser(getClass().getResourceAsStream("/day06problem1/input.txt"))));
+    }
+}

b12d3611e66a74555c742ebd31ded4adb3a65c40
mhiginio <mhiginio@gmail.com>
2023-12-10 17:15:42 +0100

    Day 9

---
+++ src/main/java/com/advent/_2023/day09problem1/Solution.java
@@ -0,0 +1,36 @@
+package com.advent._2023.day09problem1;
+
+import com.advent._2023.ParseUtils;
+import com.advent._2023.Parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public long solution(Parser parser) {
+        return parser.streamLines().mapToInt(this::extrapolationForSingleSeries).sum();
+    }
+
+    private int extrapolationForSingleSeries(String series) {
+        List<Integer> integerList = ParseUtils.asIntegerList(series);
+        List<List<Integer>> theSeriesTower = new ArrayList<>();
+        List<Integer> reducedList = integerList;
+        do {
+            theSeriesTower.add(reducedList);
+            reducedList = computeDifferences(reducedList);
+        } while (reducedList.stream().anyMatch(k -> k != 0));
+        return extrapolateNextValue(theSeriesTower);
+    }
+
+    private List<Integer> computeDifferences(List<Integer> originalList) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i < originalList.size(); i++) {
+            result.add(originalList.get(i) - originalList.get(i - 1));
+        }
+        return result;
+    }
+
+    private int extrapolateNextValue(List<List<Integer>> theSeriesTower) {
+        return theSeriesTower.stream().mapToInt(k -> k.get(k.size() - 1)).sum();
+    }
+}
---
+++ src/main/java/com/advent/_2023/day09problem2/Solution.java
@@ -0,0 +1,40 @@
+package com.advent._2023.day09problem2;
+
+import com.advent._2023.ParseUtils;
+import com.advent._2023.Parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public long solution(Parser parser) {
+        return parser.streamLines().mapToInt(this::extrapolationForSingleSeries).sum();
+    }
+
+    private int extrapolationForSingleSeries(String series) {
+        List<Integer> integerList = ParseUtils.asIntegerList(series);
+        List<List<Integer>> theSeriesTower = new ArrayList<>();
+        List<Integer> reducedList = integerList;
+        do {
+            theSeriesTower.add(reducedList);
+            reducedList = computeDifferences(reducedList);
+        } while (reducedList.stream().anyMatch(k -> k != 0));
+        return extrapolatePreviousValue(theSeriesTower);
+    }
+
+    private List<Integer> computeDifferences(List<Integer> originalList) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i < originalList.size(); i++) {
+            result.add(originalList.get(i) - originalList.get(i - 1));
+        }
+        return result;
+    }
+
+    private int extrapolatePreviousValue(List<List<Integer>> theSeriesTower) {
+        int result = 0;
+        for (int i = theSeriesTower.size() - 1; i >= 0; i--) {
+            result = theSeriesTower.get(i).get(0) - result;
+        }
+        return result;
+    }
+}

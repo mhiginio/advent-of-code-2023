c4fbff424f0453664db09ea1803267e4ff54cd1c
mhiginio <mhiginio@gmail.com>
2023-12-11 18:57:40 +0100

    Day 11

---
+++ src/main/java/com/advent/_2023/day11problem2/Solution.java
@@ -0,0 +1,10 @@
+package com.advent._2023.day11problem2;
+
+public class Solution extends com.advent._2023.day11problem1.Solution {
+
+
+    public int getGrowth() {
+        return 1000000 - 1;
+    }
+
+}

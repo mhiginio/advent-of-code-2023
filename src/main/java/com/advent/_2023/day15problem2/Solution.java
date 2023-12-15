fdfd2841aa819440f4013d692f604313ec74a49e
mhiginio <mhiginio@gmail.com>
2023-12-15 10:27:07 +0100

    Day 15

---
+++ src/main/java/com/advent/_2023/day15problem2/Solution.java
@@ -0,0 +1,88 @@
+package com.advent._2023.day15problem2;
+
+import com.advent._2023.Parser;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
+
+import java.util.*;
+
+import static java.util.Arrays.stream;
+
+public class Solution extends com.advent._2023.day15problem1.Solution {
+    private final Map<Integer, LensCollection> boxes = new HashMap<>();
+
+    public long solution(Parser parser) {
+        String[] instructions = parser.streamLines().map(k -> k.split(",")).findFirst().orElseThrow();
+        stream(instructions).forEach(this::processInstruction);
+        return computeFocusingPower();
+    }
+
+    private void processInstruction(String instruction) {
+        if (instruction.endsWith("-")) {
+            processRemoval(instruction);
+        } else {
+            processUpsert(instruction);
+        }
+    }
+
+    private void processRemoval(String instruction) {
+        String label = instruction.substring(0, instruction.length() - 1);
+        boxes.computeIfAbsent(hash(label), x -> new LensCollection()).removeByLabel(label);
+    }
+
+    private void processUpsert(String instruction) {
+        int idx = instruction.indexOf('=');
+        String label = instruction.substring(0, idx);
+        int focalLength = Integer.parseInt(instruction.substring(idx + 1));
+        boxes.computeIfAbsent(hash(label), x -> new LensCollection()).upsert(label, focalLength);
+    }
+
+    private long computeFocusingPower() {
+        return boxes.entrySet().stream().mapToInt(e -> e.getValue().computeFocusingPower(e.getKey())).sum();
+    }
+
+    @Data
+    public static final class LensCollection {
+        private final List<Lens> lens = new LinkedList<>();
+
+        public void removeByLabel(String label) {
+            lens.remove(Lens.of(label));
+        }
+
+        public void upsert(String label, int focalLength) {
+            Lens currentLens = Lens.of(label, focalLength);
+            int idx = lens.indexOf(currentLens);
+            if (idx != -1) {
+                lens.get(idx).setFocalLength(focalLength);
+            } else {
+                lens.add(currentLens);
+            }
+        }
+
+        public int computeFocusingPower(Integer boxNumber) {
+            int result = 0;
+            for (int i = 0; i < lens.size(); i++) {
+                result += (boxNumber + 1) * (i + 1) * lens.get(i).getFocalLength();
+            }
+            return result;
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    @EqualsAndHashCode(of = "label")
+    public static final class Lens {
+        private final String label;
+        private int focalLength;
+
+        public static Lens of(String label) {
+            return of(label, 0);
+        }
+
+        public static Lens of(String label, int focalLength) {
+            return new Lens(label, focalLength);
+        }
+    }
+}
---
+++ src/test/java/com/advent/_2023/day15problem1/SolutionTest.java
@@ -0,0 +1,30 @@
+package com.advent._2023.day15problem1;
+
+import com.advent._2023.InputParser;
+import com.advent._2023.StringParser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+    private final Solution solution = new Solution();
+
+    @Test
+    void hash_of_HASH_shold_return_52() {
+        assertEquals(52, solution.hash("HASH"));
+    }
+
+    @Test
+    void example_data_should_return_1320() {
+
+        String[] data = {"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"};
+        assertEquals(1320, solution.solution(new StringParser(data)));
+    }
+
+
+    @Test
+    void provided_data_should_return_511343() {
+        assertEquals(511343,
+                solution.solution(new InputParser(getClass().getResourceAsStream("/day15problem1/input.txt"))));
+    }
+}
